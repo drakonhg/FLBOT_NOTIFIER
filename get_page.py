52c263bc736b940cd28eb9f94a752dc669fef59e
drakonhg <drakonhg@gmail.com>
2021-04-08 13:14:12 +0300

    updated headers

--- get_page.py
+++ get_page.py
@@ -6,19 +6,20 @@ HEADERS = {
     'authority': 'www.fl.ru',
     'pragma': 'no-cache',
     'cache-control': 'no-cache',
-    'upgrade-insecure-requests': '1',
+    'sec-ch-ua': '"Google Chrome";v="89", "Chromium";v="89", ";Not A Brand";v="99"',
+    'sec-ch-ua-mobile': '?0',
     'dnt': '1',
-    'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Mobile Safari/537.36',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36',
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
     'sec-fetch-site': 'same-origin',
     'sec-fetch-mode': 'navigate',
     'sec-fetch-user': '?1',
     'sec-fetch-dest': 'document',
-    'sec-ch-ua': '"Google Chrome";v="89", "Chromium";v="89", ";Not\\"A\\\\Brand";v="99"',
-    'sec-ch-ua-mobile': '?1',
     'referer': 'https://www.fl.ru/projects/',
-    'accept-language': 'ru-RU,ru;q=0.9',
-    'cookie': '__ddg1=x2JF9yDrLhmOw5DqVQ4s; PHPSESSID=01d373826b6840a68e6001a52525a51b; _ga=GA1.2.1269193950.1616787796; _gid=GA1.2.682418418.1616787796; _ga_cid=1269193950.1616787796; user_unic_ac_id=170f325e-f168-e909-69a2-346e3fdc8e8b; advcake_trackid=81ba2456-b23f-9668-cdaa-d0dd71d67ea3; _ym_uid=1616787797904039157; _ym_d=1616787797; _fbp=fb.1.1616787796705.1258820415; __gads=ID=46b44d0a92a3b173-22035847e3ba001a:T=1616787796:RT=1616787796:S=ALNI_MaB2JJDMZqujJo_eC8goK9Vh90uEA; _ym_isad=2; new_pf0=1; new_pf10=1; hidetopprjlenta=0; cookies_accepted=1; __ddg2=21aIWAnwY0T19UO1; uechat_3_first_time=1616828334859; uechat_3_pages_count=2; uechat_3_disabled=true; new_pf0=1',}
+    'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+    'cookie': '_ym_uid=157468517432521403; _ga=GA1.2.618592098.1598959650; cookies_accepted=1; __ddg1=AqnvAKyGodtcxFnIgy0O; user_device_id=ul8q7rkoxn01epr30deahs1vnwq05tzj; __ddg2=xcZ0qqRwnFBir2L4; _fbp=fb.1.1612353850421.1271460300; __ddg4=6d6eb3f7074457e7c1c5e4fb06f73661; take-care-showed=%5B4676579%5D; _ym_d=1616152983; _ga_cid=618592098.1598959650; user_unic_ac_id=b3414462-bae6-fbfc-2ef1-ec4a72027912; advcake_trackid=17417063-e725-7a7b-1845-79efe9f9e6f0; new_pf0=1; new_pf10=1; PHPSESSID=df7940df9b450054b5af8ebc7375aaad; id=4538514; name=drakonhg%40gmail.com; pwd=4e7fd36de61295c3bf225ac9e2347eb4; __ddgid=1EE9BsVTyoeQVG3D; _gid=GA1.2.798844382.1617445642; __ddgmark=gPTCnT1xH3qerzJr; ue_sso_token=HacYqhWnCCs%252BUviJwLRrciuwN26lf%252FJCGaoePBC79udM9d%252FwzvrewSrHPEuQcci6w5e66dF80521FvXDG2VeZK1pTaX%252B1GZr9qm4R44kDDBBVCCKgAqksDPn1K1Ve2eYaExWaBHIAvtoGwPIOJc31uvw7%252FQN2SiXbUBXvjChM8E1vXL2URsGD6wGsEmLRTg2KBvmxtuS9rY58bu8uOmYRTVOtVKk6KgxzrsfIKMh23slhU8gGja3Vk%252Fqa6wftZ00Vv9tJq4lMsAL8WC5tc4iuK%252BPpEXwJaeeHrFr0LrJAqdb61sJVpGuaSh%252BEc%252BFwDLNq0Zb94kXQmoZ4XgmlpBbENvbdT%252FXrCf5i7zt9kznVc%252BPVhQMeNW4%252Fq6Q9x%252FolUDJtXSKmExA6wtCik2RsQ1d%252B7NKYeWDbDABvHRnS5DgmxniOjwBmy%252FPJWNSYyqc4FkTY7OrNis5A6s5WOCNiLnmaJkRz8RqImSKXlU6neeFfUbB3zLKZFYIl2Frd0BQy9YP5WwhVxYQaaeDWBrrB3%252B2wJDrUPMJQZaXMRB%252FVWXU8rM%253D; XSRF-TOKEN=FwwPCP42Ai8NgXANVDXdbezDfS6E7sAw1DruCHem; hidetopprjlenta=1; hidetopprjlenta_time=1617819820; mobapp=1617868855; _ym_isad=1; uechat_3_first_time=1617876121805; uechat_3_pages_count=49; uechat_3_disabled=true',
+}
 
 URL = "https://www.fl.ru"
 
